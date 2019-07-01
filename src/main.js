081faf537b19166891e94b51a616243bd96bf5ac
alipajand <ali.pajand@gmail.com>
2019-07-01 18:28:19 +0430

    :art: Add bootstrap-vue and axios into project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './mixins';
 
 import './plugins/axios.js';
 import './plugins/vue-bootstrap.js';
-import './plugins/vue-notify.js';
+import './plugins/vue-notification.js';
 
 import './assets/scss/bootstrap.scss';
--- src/views/public/Index.vue
+++ src/views/public/Index.vue
@@ -1,21 +1,7 @@
 <template>
-  <div class="pb-5">
-    <navigation-component v-bind:showSlogan="false"
-                          v-bind:margin="30">
-    </navigation-component>
-    <router-view/>
-    <footer-component v-bind:margin="30"></footer-component>
-  </div>
+  <router-view/>
 </template>
 
 <script type="text/javascript">
-  import footerComponent from '../shared/TheFooter';
-  import navigationComponent from '../shared/TheNavigation';
-
-  export default {
-    components: {
-      footerComponent,
-      navigationComponent
-    }
-  };
+  export default {};
 </script>
