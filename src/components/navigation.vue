5075301c6634b8d5be37fd81d371a0497f982a8e
alipajand <ali.pajand@gmail.com>
2019-07-03 12:30:27 +0430

    :art: Add global components

---
+++ src/components/navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">BSUP</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
+          <b-nav-item :to="{name: 'book'}">Books</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-on:click="logout()">Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      logout() {
+        this.$store.dispatch('auth/logout');
+      }
+    }
+  };
+</script>
